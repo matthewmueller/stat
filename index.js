91c5849fd3b1829bc8ee529659fa058a1275a451
Dominic Barnes <dominic@dbarnes.info>
2015-10-26 01:01:00 -0700

    adding debug output

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 
 'use strict';
 
+let debug = require('debug')('mako-stat');
 let fs = require('fs');
 
 module.exports = function (extensions) {
+  debug('initialize %j', extensions);
+
   return function (mako) {
     mako.preread(extensions, function stat(file, tree, mako, done) {
+      debug('checking stat for %s', file.path);
       fs.stat(file.path, function (err, stat) {
-        if (err) return done(err);
-        file.stat = stat;
-        done();
+        if (err) {
+          debug(err.message);
+          done(err);
+        } else {
+          debug('stat for %s', file.path, stat);
+          file.stat = stat;
+          done();
+        }
       });
     });
   };

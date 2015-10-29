9c59e6fcd4791fec214d0d5dda4953f41c24e68b
Dominic Barnes <dominic@dbarnes.info>
2015-10-29 00:48:39 -0700

    adding support for setting file as dirty when mtime changes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = function (extensions) {
   return function (mako) {
     mako.preread(extensions, function stat(file, tree, mako, done) {
       debug('checking stat for %s', file.path);
+      let previous = file.stat;
       fs.stat(file.path, function (err, stat) {
         if (err) {
           debug(err.message);
@@ -17,9 +18,22 @@ module.exports = function (extensions) {
         } else {
           debug('stat for %s', file.path, stat);
           file.stat = stat;
+          file.analyzed = !modified(previous, stat);
           done();
         }
       });
     });
   };
 };
+
+/**
+ * Compare two `fs.Stat` objects to see if the `mtime` has changed.
+ *
+ * @param {fs.Stat} previous  The last stat object.
+ * @param {fs.Stat} current   The new stat object.
+ * @return {Boolean}
+ */
+function modified(previous, current) {
+  if (!previous) return true;
+  return previous.mtime.getTime() !== current.mtime.getTime();
+}
--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ let chai = require('chai');
 let mako = require('mako');
 let path = require('path');
 let stat = require('..');
+let touch = require('touch').sync;
 
 chai.use(require('chai-as-promised'));
 let assert = chai.assert;
@@ -12,17 +13,34 @@ let fixture = path.resolve.bind(path, __dirname, 'fixtures');
 
 describe('stat plugin', function () {
   it('should add the stat object for the input file', function () {
+    let entry = fixture('simple/index.js');
+
     return mako()
       .use(stat('js'))
-      .use(function (bundl) {
-        bundl.preread('js', function (file) {
-          assert(file.stat);
-        });
-      })
-      .analyze(fixture('simple/index.js'));
+      .analyze(entry)
+      .then(function (tree) {
+        let file = tree.getFile(entry);
+        assert(file.stat);
+      });
   });
 
   it('should fail when the file does not exist', function () {
     return assert.isRejected(mako().use(stat('js')).analyze(fixture('does-not-exist.js')));
+  });
+
+  it('should analyze the file again if the mtime has changed', function () {
+    let entry = fixture('simple/index.js');
+    let processed = [];
+
+    let builder = mako().use(stat('js'));
+
+    builder.read('js', function (file) {
+      processed.push(file.path);
+    });
+
+    return builder.analyze(entry)
+      .then(() => touch(entry))
+      .then(() => builder.analyze(entry))
+      .then(() => assert.deepEqual(processed, [ entry, entry ]));
   });
 });
